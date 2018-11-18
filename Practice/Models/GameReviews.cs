321d7ea8ed68d68ed434ae7c9dcfc0d80331fbee
Thomas Miller <Tmiller907@gmail.com>
2018-11-18 15:38:31 -0600

    Updated the models page

---
+++ Practice/Models/GameReviews.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Practice.Models
+{
+    public class GameReview
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public List<string> Pictures { get; set; }
+        public int Stars { get; set; }
+        public string MainPicture
+        {
+            get
+            {
+                return Name.ToLower().Replace(" ", "").Replace("'", "").Replace(".", "") + ".jpg";
+            }
+        }
+    }
+}
